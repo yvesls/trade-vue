6c0f39f1246b21ed3b13cbb87ddc28b54c17e502
AylerF <ayler42@gmail.com>
2023-12-07 02:12:06 -0300

    Mudei umas parada ai Yves

---
+++ src/components/CadastroComprador.vue
@@ -0,0 +1,114 @@
+<template>
+    <div>
+      <h1>Login e identificação</h1>
+      <form class="input-container" @submit.prevent="entrar">
+        <label for="cpf">CPF:</label>
+        <input v-model="cpf" type="text" id="cpf" name="cpf" required>
+        <label for="nome">Nome:</label>
+        <input v-model="nome" type="text" id="nome" name="nome" required>
+        <label for="investimento">Valor total de investimento:</label>
+        <input v-model="investimento" type="number" id="investimento" name="investimento" min="0" step="0.01" required>
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        cpf: '',
+        nome: '',
+        investimento: 0,
+      };
+    },
+    methods: {
+      entrar() {
+    
+        console.log('Entrando com CPF:', this.cpf, 'e nome:', this.nome, 'Investimento:', this.investimento);
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+   body {
+            font-family: Arial, sans-serif;
+        }
+
+        h1, h2 {
+            text-align: center;
+        }
+
+        form {
+            margin: 20px auto;
+            width: 500px;
+            border: 1px solid #ccc;
+            padding: 10px;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 5px;
+        }
+
+        input {
+            display: block;
+            width: 100%;
+            margin-bottom: 10px;
+        }
+
+        button {
+            display: block;
+            width: 100%;
+            background-color: #0099ff;
+            color: white;
+            border: none;
+            padding: 10px;
+        }
+
+        table {
+            margin: 20px auto;
+            width: 500px;
+            border-collapse: collapse;
+        }
+
+        th, td {
+            border: 1px solid #ccc;
+            padding: 10px;
+            text-align: left;
+        }
+        
+        .input-container {
+            margin-bottom: 20px;
+            border-radius: 5px;
+        }
+
+        .input-container input {
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            outline: none;
+            transition: border-color 0.3s ease-in-out;
+        }
+
+        .input-container input:focus {
+            border-color: #5f9ea0;
+        }
+
+        /* Estilo do botão */
+        .input-container button {
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            background-color: #5f9ea0;
+            color: white;
+            cursor: pointer;
+            transition: background-color 0.3s ease-in-out;
+        }
+
+        .input-container button:hover {
+            background-color: #456268;
+        }
+</style>
+  
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,85 @@
+<template>
+    <div>
+      <h1>Dashboard</h1>
+      <div>
+        <h2>Resumo do Investidor</h2>
+        <p>Nome: {{ nome }}</p>
+        <p>CPF: {{ cpf }}</p>
+        <p>Investimento Total: R$ {{ investimento.toFixed(2) }}</p>
+      </div>
+      <TabelaAcoesCompradas />
+      <TabelaAcoesDisponiveis />
+    </div>
+  </template>
+  
+  <script>
+  import TabelaAcoesCompradas from '@/components/TabelaAcoesCompradas.vue';
+  import TabelaAcoesDisponiveis from '@/components/TabelaAcoesDisponiveis.vue';
+  
+  export default {
+    components: {
+      TabelaAcoesCompradas,
+      TabelaAcoesDisponiveis,
+    },
+    data() {
+      return {
+        cpf: '',
+        nome: '',
+        investimento: 0,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+   body {
+            margin: 0;
+            font-family: Arial, sans-serif;
+        }
+        .dashboard {
+            color: white;
+            position: fixed;
+            bottom: -1px;
+            left: 10%;
+            width: 80%;
+            background-color: #456268;
+            box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1);
+            overflow: hidden;
+            border-radius: 5px;
+            display: flex;
+            flex-direction: column;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 10px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+        th {
+            background-color: #5f9ea0;
+        }
+        .total-row {
+            font-weight: bold;
+        }
+        
+        .empresas-container {
+            max-height: 150px;
+            overflow-y: auto;
+        }
+
+        .valores-empresa thead {
+            position: sticky;
+            top: 0;
+            background-color: #f5f5f5;
+        }
+        .valores-empresa tfoot {
+            position: sticky;
+            bottom: 0;
+            background-color: #202222;
+            box-shadow: -2px -1px white;
+        }
+  </style>
+  
