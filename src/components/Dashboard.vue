6dee8bf540d9e2730fd3293d75ff6ae6cf81d41a
AylerF <ayler42@gmail.com>
2023-12-07 02:38:35 -0300

    Alteração de layout

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,85 +1,71 @@
 <template>
-    <div>
+  <div>
+    <div class="header">
       <h1>Dashboard</h1>
-      <div>
+    </div>
+    <div class="dashboard-content">
+      <div class="resumo">
         <h2>Resumo do Investidor</h2>
         <p>Nome: {{ nome }}</p>
         <p>CPF: {{ cpf }}</p>
         <p>Investimento Total: R$ {{ investimento.toFixed(2) }}</p>
       </div>
-      <TabelaAcoesCompradas />
-      <TabelaAcoesDisponiveis />
+      <TabelaAcoesCompradas :acoes="acoesCompradas" @venderAcao="venderAcao" />
     </div>
-  </template>
-  
-  <script>
-  import TabelaAcoesCompradas from '@/components/TabelaAcoesCompradas.vue';
-  import TabelaAcoesDisponiveis from '@/components/TabelaAcoesDisponiveis.vue';
-  
-  export default {
-    components: {
-      TabelaAcoesCompradas,
-      TabelaAcoesDisponiveis,
-    },
-    data() {
-      return {
-        cpf: '',
-        nome: '',
-        investimento: 0,
-      };
-    },
-  };
-  </script>
-  
-  <style scoped>
-   body {
-            margin: 0;
-            font-family: Arial, sans-serif;
-        }
-        .dashboard {
-            color: white;
-            position: fixed;
-            bottom: -1px;
-            left: 10%;
-            width: 80%;
-            background-color: #456268;
-            box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1);
-            overflow: hidden;
-            border-radius: 5px;
-            display: flex;
-            flex-direction: column;
-        }
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-        th, td {
-            padding: 10px;
-            text-align: left;
-            border-bottom: 1px solid #ddd;
-        }
-        th {
-            background-color: #5f9ea0;
-        }
-        .total-row {
-            font-weight: bold;
-        }
-        
-        .empresas-container {
-            max-height: 150px;
-            overflow-y: auto;
-        }
+    <TabelaAcoesDisponiveis />
+  </div>
+</template>
 
-        .valores-empresa thead {
-            position: sticky;
-            top: 0;
-            background-color: #f5f5f5;
+<script>
+import TabelaAcoesCompradas from '@/components/TabelaAcoesCompradas.vue';
+import TabelaAcoesDisponiveis from '@/components/TabelaAcoesDisponiveis.vue';
+
+export default {
+  components: {
+    TabelaAcoesCompradas,
+    TabelaAcoesDisponiveis,
+  },
+  data() {
+    return {
+      cpf: '',
+      nome: '',
+      investimento: 0,
+      acoesCompradas: [
+        {
+          empresa: 'Itaú',
+          quantidade: 10,
+          valor: 'R$ 300,00',
+          comprada: true
         }
-        .valores-empresa tfoot {
-            position: sticky;
-            bottom: 0;
-            background-color: #202222;
-            box-shadow: -2px -1px white;
-        }
-  </style>
-  
+        // Adicione mais ações compradas conforme necessário
+      ]
+    };
+  },
+  methods: {
+    venderAcao(index) {
+      // Lógica para vender ação
+      this.acoesCompradas.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Estilos como antes */
+
+.header {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.dashboard-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.resumo {
+  margin-right: 20px;
+}
+
+</style>
